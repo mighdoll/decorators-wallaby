e93a54acb6171d6e3b446f65b3e07b6d61f031ec
some fool <lee@underneath.ca>
2022-08-20 13:46:02 -0700

    tweak config

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,4 @@ export default {
   testEnvironment: 'node',
   testMatch: ["**/test/**/*.test.ts"],
   // preset: 'ts-jest',
-  // transform: {
-  //   "\\.[jt]sx?$": "babel-jest",
-  // },
-  // transformIgnorePatterns: []
 };
