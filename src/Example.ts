3a38473b3acd34dec4a757fb9e491203a3047d7f
some fool <lee@underneath.ca>
2022-08-20 14:11:59 -0700

    where is the logging?

--- src/Example.ts
+++ src/Example.ts
@@ -1,5 +1,7 @@
 export function Awesome(howAwesome: any) {
+  console.log("where is..");
   return function (target: any) {
+    console.log("logging?");
     target.prototype.howAwesome = howAwesome;
   };
 }
